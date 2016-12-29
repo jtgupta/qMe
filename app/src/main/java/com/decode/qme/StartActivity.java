b5c454485dd51405b4bdfb89e6f1323fae41a883
jatin-jt <sachinjatingupta@gmail.com>
2016-12-29 13:58:42 +0530

    Added authentication

---
+++ app/src/main/java/com/decode/qme/StartActivity.java
@@ -0,0 +1,54 @@
+package com.decode.qme;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class StartActivity extends AppCompatActivity {
+
+    private static final String TAG = "StartActivity";
+    public static final int RC_SIGN_IN = 151;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+
+        if (auth.getCurrentUser() != null) {
+            finish();
+            Intent i = new Intent(this, MainActivity.class);
+            startActivity(i);
+        }
+        else {
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setIsSmartLockEnabled(false)
+                            .setTheme(R.style.AppTheme)
+                            .setProviders(AuthUI.GOOGLE_PROVIDER)
+                            .build(),RC_SIGN_IN);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Log.d(TAG, "onActivityResult: added a user");
+                finish();
+                Intent i = new Intent(this, MainActivity.class);
+                startActivity(i);
+            } else {
+                // user is not signed in. Maybe just wait for the user to press
+                // "sign in" again, or show a message
+            }
+        }
+    }
+}
